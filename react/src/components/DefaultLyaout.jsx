5862bfc2e1d03cb8e7e376abe2f23eb88cde7e43
Amzil Mustapha <mustaphaamzil124@gmail.com>
2023-05-31 07:11:31 +0100

    user CRUD API (backend)

--- react/src/components/DefaultLyaout.jsx
+++ react/src/components/DefaultLyaout.jsx
@@ -1,15 +1,29 @@
 import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useEffect } from "react";
+import axios from "axios";
+import axiosClient from "../axios.client";
 
 function DefaultLyaout() {
-    const { user, token } = useStateContext();
+    const { user, token, setUser, setToken } = useStateContext();
 
     if (!token) {
         return <Navigate to="/login" />;
     }
     const onLogout = (e) => {
         e.preventDefault();
+
+        axiosClient.post("/logout").then(() => {
+            setUser({});
+            setToken(null);
+        });
     };
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
 
     return (
         <div id="defaultLayout">
--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axiosClient from "../axios.client";
 
 function Users() {
-  return (
-    <div>U</div>
-  )
+    const [user, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    const getUser = () => {
+        setLoading(true);
+        axiosClient
+            .get("/users")
+            .then(({ data }) => {
+                setLoading(false);
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    };
+
+    return <div>U</div>;
 }
 
 export default Users;
