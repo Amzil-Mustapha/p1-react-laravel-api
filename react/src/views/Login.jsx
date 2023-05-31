3e177af6f69dd4a6e0dd87b3322ee80a0d6e1800
Amzil Mustapha <mustaphaamzil124@gmail.com>
2023-05-31 06:09:04 +0100

    implment login

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,16 +1,56 @@
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios.client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { setUser, setToken } = useStateContext();
+    const [errors, setErrors] = useState(null);
+
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        };
+        axiosClient
+            .post("/login", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status == 422) {
+                    setErrors(response.data.errors);
+                    console.log(response.data.errors);
+                }
+            });
     };
+
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Login into your account</h1>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+
+                    {errors && (
+                        <div className="alert">
+                            {Object.keys(errors).map((key) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
+
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
                     <button className="btn btn-block"> Login</button>
                     <p className="message">
                         Not Registered?{" "}
